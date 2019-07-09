ba2e0862362bb95774c7ccc03d78a462c13af2f2
akrpv <al.kurapov@gmail.com>
2019-07-09 23:26:15 +0300

    dateTimeAdapter

---
+++ project/src/main/java/second/DateTimeAdapter.java
@@ -0,0 +1,20 @@
+package second;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class DateTimeAdapter extends XmlAdapter<String, Date> {
+
+    private final DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+    @Override
+    public Date unmarshal(String xml) throws Exception {
+        return dateFormat.parse(xml);
+    }
+
+    @Override
+    public String marshal(Date object) throws Exception {
+        return dateFormat.format(object);
+    }
+}
